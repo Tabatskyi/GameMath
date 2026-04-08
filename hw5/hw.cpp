e9da06f5a21609cea0cc0d8416c154862a456401
Mark Tabatskyi <mark.tabatskyi@gmail.com>
2026-04-08 12:56:28 +0300

    completed add-sub

--- hw5/hw.cpp
+++ hw5/hw.cpp
@@ -11,3 +11,26 @@ float vector4::y() const { return _y; }
 float vector4::z() const { return _z; }
 float vector4::w() const { return _w; }
 
+vector4& vector4::add(const vector4 &other)
+{
+    _data = _mm_add_ps(_data, other._data);
+    return *this;
+}
+
+vector4& vector4::add(float x, float y, float z)
+{
+    _data = _mm_add_ps(_data, _mm_setr_ps(0.0f, x, y, z));
+    return *this;
+}
+
+vector4& vector4::sub(const vector4 &other)
+{
+    _data = _mm_sub_ps(_data, other._data);
+    return *this;
+}
+
+vector4& vector4::sub(float x, float y, float z)
+{
+    _data = _mm_sub_ps(_data, _mm_setr_ps(0.0f, x, y, z));
+    return *this;
+}
